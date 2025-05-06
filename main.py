6a55171e316fb9d78ffbfb94dcd1600cf2841e21
Enrico Di Bacco <enrico.dibacco@gmail.com>
2025-05-06 20:31:47 +0200

    Part of previous commit

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from player import Player
 pg.init()
 screen = pg.display.set_mode((1920,1080))
 clock = pg.time.Clock()
+
+all_sprites = pg.sprite.Group()
+all_sprites.add(Player)
+
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -13,6 +17,11 @@ while True:
             if event.key == pg.K_ESCAPE:
                 pg.quit()
                 exit()
+
+    #here i draw the background and the player
+    screen.fill((0, 0, 0))
+        
+
 
     pg.display.update()
     clock.tick(60)
