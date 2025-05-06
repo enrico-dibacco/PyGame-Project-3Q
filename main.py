0d124ad8935e4a36d71827f4d73f3337b8402283
Enrico Di Bacco <enrico.dibacco@gmail.com>
2025-05-06 21:19:38 +0200

    FInishing up the movement and getting the keys for the player movement

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from player import Player
 pg.init()
 screen = pg.display.set_mode((1920,1080))
 clock = pg.time.Clock()
-
-all_sprites = pg.sprite.Group()
-all_sprites.add(Player)
+player = Player((960, 540))
+all_sprites = pg.sprite.Group() #i use the group to manage all the sprites and i want to make collisions easier to handle
+all_sprites.add(player)
 
 while True:
+    
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
@@ -17,10 +18,11 @@ while True:
             if event.key == pg.K_ESCAPE:
                 pg.quit()
                 exit()
-
+    keys = pg.key.get_pressed() #i get the keys pressed to move the player
+    all_sprites.update(keys)
     #here i draw the background and the player
     screen.fill((0, 0, 0))
-        
+    all_sprites.draw(screen)    
 
 
     pg.display.update()
